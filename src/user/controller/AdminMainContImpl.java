f809678987d50fcaa36f83fc0d7f4bca85caeaea
Eveieve <jhkdev1@gmail.com>
2025-03-19 19:13:03 +0900

    feat: 총관리자의 1~6 메뉴 진입점 추가

--- src/user/controller/AdminMainContImpl.java
+++ src/user/controller/AdminMainContImpl.java
@@ -16,12 +16,53 @@ public class AdminMainContImpl {
     public void start() throws SQLException {
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            printFirstMenu();
+            String firstMenu = sc.nextLine();
 
+            try {
+                firstMenu = scanner.nextLine().trim(); // 문자열 입력 방지
+            } catch (NumberFormatException e) {
+                System.out.println("잘못된 입력입니다. 숫자를 입력하세요.");
+                continue;
+            }
 
-          printMenu();
-            // if user... 추가하기
-            String choice = scanner.nextLine();
+            switch (firstMenu) {
+                case "1":
+                    printMenu();
+                    startUserManageMenu();
 
+                    break;
+                case "2":
+                    System.out.println("재무 관리 메뉴로 이동합니다.");
+                    break;
+                case "3":
+                    System.out.println("창고 관리 메뉴로 이동합니다.");
+                    break;
+                case "4":
+                    System.out.println("재고 관리 메뉴로 이동합니다.");
+                    break;
+                case "5":
+                    System.out.println("입고 관리 메뉴로 이동합니다.");
+                    break;
+                case "6":
+                    System.out.println("출고 관리 메뉴로 이동합니다.");
+                    break;
+                case "0":
+                    System.out.println("프로그램을 종료합니다.");
+                    scanner.close();
+                    return; // while 문 탈출
+                default:
+                    System.out.println("잘못된 선택입니다. 0~6 사이의 숫자를 입력하세요.");
+            }
+
+        }
+    }
+
+    private void startUserManageMenu() throws SQLException {
+
+        String choice = sc.nextLine().trim();
+
+        while(true) {
             switch (choice) {
                 case "1" -> createUser();
                 case "2" -> findUser();
@@ -29,11 +70,24 @@ public class AdminMainContImpl {
                 case "4" -> deleteUser();
                 case "0" -> {
                     System.out.println("프로그램을 종료합니다.");
-                    return;
+                    System.exit(0);
                 }
                 default -> System.out.println("유효하지 않은 입력입니다. 재입력하세요.");
             }
         }
+
+    }
+
+    private void printFirstMenu() {
+        System.out.println("\n===== 메인 메뉴 =====");
+        System.out.println("1. 회원 관리");
+        System.out.println("2. 재무 관리");
+        System.out.println("3. 창고 관리");
+        System.out.println("4. 재고 관리");
+        System.out.println("5. 입고 관리");
+        System.out.println("6. 출고 관리");
+        System.out.println("0. 종료");
+        System.out.print("메뉴를 선택하세요: ");
     }
 
     private void printMenu() {
